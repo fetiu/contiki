c3785ee0ded9410db071cbb2cbc1b3b8c9588fdb
Jake Kim <jake@getawair.com>
2022-11-08 17:16:47 +0900

    cleanup uneeded codes
    unsure why it's unstable after deleting static variable `buf`

--- examples/cc2530dk/udp-ipv6/server.c
+++ examples/cc2530dk/udp-ipv6/server.c
@@ -34,11 +34,7 @@
 #include <string.h>
 
 #define DEBUG DEBUG_PRINT
-#include "net/ip/uip-debug.h"
-#include "dev/watchdog.h"
 #include "dev/leds.h"
-#include "net/rpl/rpl.h"
-#include "dev/button-sensor.h"
 #include "debug.h"
 
 #define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
@@ -48,20 +44,10 @@
 
 static struct uip_udp_conn *server_conn;
 static char buf[MAX_PAYLOAD_LEN];
-// static uint16_t len;
 
-#define SERVER_REPLY          0
-
-/* Should we act as RPL root? */
-#define SERVER_RPL_ROOT       0
-
-#if SERVER_RPL_ROOT
-static uip_ipaddr_t ipaddr;
-#endif
-/*---------------------------------------------------------------------------*/
 PROCESS(udp_server_process, "UDP server process");
 AUTOSTART_PROCESSES(&udp_server_process);
-/*---------------------------------------------------------------------------*/
+
 static void
 print_new_src(uip_ip6addr_t *new)
 {
@@ -80,116 +66,30 @@ print_new_src(uip_ip6addr_t *new)
 static void
 tcpip_handler(void)
 {
-  memset(buf, 0, MAX_PAYLOAD_LEN);
   if(uip_newdata()) {
     leds_on(LEDS_GREEN);
     print_new_src(&UIP_IP_BUF->srcipaddr);
-    // len = uip_datalen();
     putstring(uip_appdata);
-#if SERVER_REPLY
-    uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
-    server_conn->rport = UIP_UDP_BUF->srcport;
-
-    uip_udp_packet_send(server_conn, buf, len);
-    /* Restore server connection to allow data from any node */
-    uip_create_unspecified(&server_conn->ripaddr);
-    server_conn->rport = 0;
-#endif
   }
   leds_off(LEDS_GREEN);
   return;
 }
-/*---------------------------------------------------------------------------*/
-#if (BUTTON_SENSOR_ON && (DEBUG==DEBUG_PRINT))
-static void
-print_stats()
-{
-  PRINTF("tl=%lu, ts=%lu, bs=%lu, bc=%lu\n",
-         RIMESTATS_GET(toolong), RIMESTATS_GET(tooshort),
-         RIMESTATS_GET(badsynch), RIMESTATS_GET(badcrc));
-  PRINTF("llrx=%lu, lltx=%lu, rx=%lu, tx=%lu\n", RIMESTATS_GET(llrx),
-         RIMESTATS_GET(lltx), RIMESTATS_GET(rx), RIMESTATS_GET(tx));
-}
-#endif
-/*---------------------------------------------------------------------------*/
-static void
-print_local_addresses(void)
-{
-  int i;
-  uint8_t state;
 
-  PRINTF("Server IPv6 addresses:\n");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
-    state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused && (state == ADDR_TENTATIVE || state
-        == ADDR_PREFERRED)) {
-      PRINTF("  ");
-      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
-      PRINTF("\n");
-      if(state == ADDR_TENTATIVE) {
-        uip_ds6_if.addr_list[i].state = ADDR_PREFERRED;
-      }
-    }
-  }
-}
-/*---------------------------------------------------------------------------*/
-#if SERVER_RPL_ROOT
-void
-create_dag()
-{
-  rpl_dag_t *dag;
-
-  uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
-  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
-
-  print_local_addresses();
-
-  dag = rpl_set_root(RPL_DEFAULT_INSTANCE,
-                     &uip_ds6_get_global(ADDR_PREFERRED)->ipaddr);
-  if(dag != NULL) {
-    uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
-    rpl_set_prefix(dag, &ipaddr, 64);
-    PRINTF("Created a new RPL dag with ID: ");
-    PRINT6ADDR(&dag->dag_id);
-    PRINTF("\n");
-  }
-}
-#endif /* SERVER_RPL_ROOT */
-/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_server_process, ev, data)
 {
-
   PROCESS_BEGIN();
-  // putstring("Starting UDP server\n");
-
-#if BUTTON_SENSOR_ON
-  putstring("Button 1: Print RIME stats\n");
-#endif
-
-#if SERVER_RPL_ROOT
-  create_dag();
-#endif
 
   // Disable transmission to nowent crash
   tcpip_set_outputfunc(NULL);
-
   server_conn = udp_new(NULL, UIP_HTONS(0), NULL);
   udp_bind(server_conn, UIP_HTONS(3000));
-
-  PRINTF("Listen port: 3000, TTL=%u\n", server_conn->ttl);
 
   while(1) {
     PROCESS_YIELD();
     if(ev == tcpip_event) {
       tcpip_handler();
-#if (BUTTON_SENSOR_ON && (DEBUG==DEBUG_PRINT))
-    } else if(ev == sensors_event && data == &button_sensor) {
-      print_stats();
-#endif /* BUTTON_SENSOR_ON */
     }
   }
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
